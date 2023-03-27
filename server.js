7ec7dce1bdae01c53617e3d78dc10e48ea99f51f
BrooklynItalian <97080240+BrooklynItalian@users.noreply.github.com>
2023-03-27 19:41:39 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+
+const app = express();
+
+const path = require('path');
+
+var http = require('http');
+var fs = require('fs');
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+
+app.use(express.static('public'))
+
+// simple route
+app.get("/", (req, res) => {
+  // res.sendFile(path.join(__dirname+'/index.html'));
+  // res.json({ message: "Hello Professor." });
+  res.send('Hello')
+});
+
+require("./app/routes/user.routes.js")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
